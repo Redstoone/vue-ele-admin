30755580fe180bc7fd75d88758cae558814cfd5e
唐成亮 <cy_jing_2010@163.com>
2017-07-26 17:53:16 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
+/* login */
 import Hello from '@/components/Hello'
+
 
 Vue.use(Router)
 
 export default new Router({
+  // mode: 'history', //后端支持可开
+  scrollBehavior: () => ({ y: 0 }),
   routes: [
     {
       path: '/',
