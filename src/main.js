5e2f8c51de8002a3da2f37c029bf497d97b0bc81
唐成亮 <cy_jing_2010@163.com>
2017-07-26 08:09:48 +0800

    添加必要组件

--- src/main.js
+++ src/main.js
@@ -3,13 +3,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+import * as filters from './filters' // 全局vue filter
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+Vue.use(ElementUI)
+
+// 注册全局 filter
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
+
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
-})
+  store,
+  render: h => h(App)
+}).$mount('#app')
